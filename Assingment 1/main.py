aae8a2352961eec9188c79b85770609e94db0730
tvu245KA22CS43 <144363537+tvu245KA22CS43@users.noreply.github.com>
2023-09-07 21:07:51 +0530

    Update main.py

--- Assingment 1/main.py
+++ Assingment 1/main.py
@@ -17,3 +17,9 @@ def squaresum(n):
     sm = 0
     for i in range(1, n+1):
         sm = sm + (i * i)
+     x = int(1)
+     y = int(2.8)
+     z = int("3")
+     print(x)
+     print(y)
+     print(z)
